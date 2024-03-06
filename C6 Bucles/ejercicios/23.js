72888f0c51c064fcdbed7ca294980faa89341c80
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-06 16:12:02 -0500

    C6 -> 23.js solucionado

--- C6 Bucles/ejercicios/23.js
+++ C6 Bucles/ejercicios/23.js
@@ -3,6 +3,12 @@ function doWhile(num) {
   // Retorna el valor final.
   // PISTA: Utiliza el bucle do-while.
   // Tu código:
+  let contador = 1;
+  do {
+    num += 5;
+    contador += 1;
+  } while (contador <= 8);
+  return num;
 }
 
 module.exports = doWhile;
