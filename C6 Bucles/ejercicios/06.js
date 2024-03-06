2d6c00a8e608ac9f5d692030829c7ae0ee258228
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-06 14:37:55 -0500

    C6 -> 06.js solucionado

--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,7 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  return num.toString().length == 3 ? true : false;
 }
 
 module.exports = tieneTresDigitos;
