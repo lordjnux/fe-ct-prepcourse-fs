321cd69375452597b8bfb44a89db35a9683c9312
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-07 14:08:40 -0500

    C7 -> 22.js solucionado

--- C7 Arrays/ejercicios/22.js
+++ C7 Arrays/ejercicios/22.js
@@ -1,7 +1,13 @@
 function tablaDelSeis() {
-  // Devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
+  // Devuelve un tablaSeis con los resultados de la tabla de multiplicar del 6 en orden creciente.
   // Desde 0 al 60.
   // Tu código:
+  var tablaSeis = [];
+  for (let i = 0; i <= 10; i++) {
+    tablaSeis.push(i * 6);
+  }
+  console.log(tablaSeis);
+  return tablaSeis;
 }
 
 module.exports = tablaDelSeis;
