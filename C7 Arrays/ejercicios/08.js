8922c2102b1021e9df2ee3cac9f654df80a76099
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-07 10:00:50 -0500

    C7 -> 08.js solucionado

--- C7 Arrays/ejercicios/08.js
+++ C7 Arrays/ejercicios/08.js
@@ -1,8 +1,9 @@
 function encontrarElemento(num, array) {
-   // Busca el número pasado por argumento dentro del array.
-   // Si lo encuentras debes retornar el INDICE en el que se encuentra dentro del array.
-   // Si no se encuentra, retorna -1.
-   // Tu código:
+  // Busca el número pasado por argumento dentro del array.
+  // Si lo encuentras debes retornar el INDICE en el que se encuentra dentro del array.
+  // Si no se encuentra, retorna -1.
+  // Tu código:
+  return array.findIndex((x) => x === num);
 }
 
 module.exports = encontrarElemento;
