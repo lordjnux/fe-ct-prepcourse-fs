74df7922d88582c129d1639598dde77a192dda8b
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-07 16:10:38 -0500

    C7 -> 30.js solucionado

--- C7 Arrays/ejercicios/30.js
+++ C7 Arrays/ejercicios/30.js
@@ -2,6 +2,14 @@ function encontrarElementoRepetido(numeros) {
   // La función recibe un argumento "numeros" que es un array de números.
   // Retorna el primer elemento repetido que se encuentre en el array.
   // Tu código:
+  for (let i = 0; i < numeros.length; i++) {
+    for (let j = 0; j < numeros.length; j++) {
+      if (numeros[i] == numeros[j] && i != j) {
+        return numeros[i];
+      }
+    }
+  }
+  return undefined;
 }
 
 module.exports = encontrarElementoRepetido;
