da60e13f1d804af5dff64500159d4476aa042d5f
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-07 11:03:56 -0500

    C7 -> 15.js solucionado

--- C7 Arrays/ejercicios/15.js
+++ C7 Arrays/ejercicios/15.js
@@ -2,6 +2,10 @@ function encontrarIndiceMayor(array) {
   // Encuentra el índice del número más grande en el array de números.
   // Devuelve el valor de este índice.
   // Tu código:
+  if (array.length === 0) return 0;
+  let bigMaxElement = Math.max(...array);
+  return array.findIndex((x) => x == bigMaxElement);
+  
 }
 
 module.exports = encontrarIndiceMayor;
