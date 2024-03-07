86e19522b0888d60605e306aa7a72aaf40574226
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-07 11:06:08 -0500

    C7 -> 16.js solucionado

--- C7 Arrays/ejercicios/16.js
+++ C7 Arrays/ejercicios/16.js
@@ -2,6 +2,7 @@ function multiplicarElementosPorIndice(array) {
   // Multiplica cada elemento del array por su índice.
   // Devuelve el nuevo arreglo con los resultados.
   // Tu código:
+  return array.map((xElement, index) => xElement * index);
 }
 
 module.exports = multiplicarElementosPorIndice;
