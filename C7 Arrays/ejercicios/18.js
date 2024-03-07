9567381490ed060d937845ed376c090d19e5fec4
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-07 11:10:30 -0500

    C7 -> 18.js solucionado

--- C7 Arrays/ejercicios/18.js
+++ C7 Arrays/ejercicios/18.js
@@ -1,6 +1,12 @@
 function promedioResultadosTest(resultadosTest) {
   // Itera sobre los elementos del arreglo resultadosTest y devuelve el promedio de las notas.
   // Tu código:
+  if (resultadosTest.length == 0) return 0;
+  if (resultadosTest.length == 1) return resultadosTest[0];
+  
+  let total = 0;
+  resultadosTest.forEach((x) => { total += x});
+  return total / resultadosTest.length;
 }
 
 module.exports = promedioResultadosTest;
