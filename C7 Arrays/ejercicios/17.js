ae55288bd06da3f4e4b863a462cda5fe7d804a6b
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-07 11:07:29 -0500

    C7 -> 17.js solucionado

--- C7 Arrays/ejercicios/17.js
+++ C7 Arrays/ejercicios/17.js
@@ -1,6 +1,9 @@
 function agregarNumeros(arrayOfNums) {
   // Suma todos los elementos de arrayOfNums y retorna el resultado.
   // Tu código:
+  let total = 0;
+  arrayOfNums.forEach((num) => total += num);
+  return total;
 }
 
 module.exports = agregarNumeros;
