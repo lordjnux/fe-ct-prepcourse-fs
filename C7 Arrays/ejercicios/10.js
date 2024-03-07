ae5064475145d8948314665b03783d35a696f780
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-07 10:35:01 -0500

    C7 -> 09.js solucionado + M7T10.test.js corregido

--- C7 Arrays/ejercicios/10.js
+++ C7 Arrays/ejercicios/10.js
@@ -1,6 +1,7 @@
 function obtenerPrimerStringLargo(array) {
   // Devuelve el primer string con más de 5 caracteres en el array.
   // Tu código:
+  return array.find((x) => x.length > 5)
 }
 
 module.exports = obtenerPrimerStringLargo;
--- C7 Arrays/tests/M7T10.test.js
+++ C7 Arrays/tests/M7T10.test.js
@@ -3,8 +3,8 @@ const obtenerPrimerStringLargo = require('../ejercicios/10');
 describe('obtenerPrimerStringLargo', () => {
    it('should return the first string with more than 5 characters', () => {
       expect(
-         obtenerPrimerStringLargo(['hello', 'world', 'this', 'is', 'a', 'test'])
-      ).toBe('hello');
+         obtenerPrimerStringLargo(['hellos', 'world', 'this', 'is', 'a', 'test'])
+      ).toBe('hellos');
       expect(
          obtenerPrimerStringLargo([
             'this',
@@ -21,7 +21,7 @@ describe('obtenerPrimerStringLargo', () => {
 
    it('should return undefined if no string with more than 5 characters is found', () => {
       expect(
-         obtenerPrimerStringLargo(['hi', 'there', 'how', 'are', 'you'])
+         obtenerPrimerStringLargo(['hi', 'ther', 'how', 'are', 'you'])
       ).toBeUndefined();
       expect(
          obtenerPrimerStringLargo(['this', 'is', 'a', 'test'])
