8f500a1697490800ad75b438901f180120fbecde
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-07 15:07:19 -0500

    C7 -> 26.js solucionado + M7T26.test.js corregido

--- C7 Arrays/ejercicios/26.js
+++ C7 Arrays/ejercicios/26.js
@@ -3,6 +3,16 @@ function encontrarPrimerMultiploDeN(n, secuencia) {
   // Devuelve el primer múltiplo de n que encuentres en la secuencia de números.
   // Usa un break para detener el bucle una vez halles el múltiplo.
   // Tu código:
+  let i = 0;
+  let multiplo = undefined;
+  while (i < secuencia.length) {
+    if (secuencia[i] % n == 0) {
+      multiplo = secuencia[i];
+      break;
+    }
+    i++;
+  }
+  return multiplo;
 }
 
 module.exports = encontrarPrimerMultiploDeN;
