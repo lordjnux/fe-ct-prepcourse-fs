828710309c4959d146a1f96201f8d4050da4e260
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-07 11:13:39 -0500

    C7 -> 11.js solucionado

--- C7 Arrays/ejercicios/11.js
+++ C7 Arrays/ejercicios/11.js
@@ -2,7 +2,7 @@ function duplicarElementos(array) {
   // Duplica (multiplica x2) cada elemento del array de números.
   // Devuelve un array con los duplicados.
   // Tu código:
-  return array.map((x) => x * 2)
+  return array.map((x) => x * 2);
 }
 
 module.exports = duplicarElementos;
