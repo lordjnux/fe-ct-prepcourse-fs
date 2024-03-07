7030ab6706ea94ab0ce535d909765296ace5c25f
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-07 10:13:18 -0500

    C7 -> 09.js solucionado

--- C7 Arrays/ejercicios/09.js
+++ C7 Arrays/ejercicios/09.js
@@ -1,7 +1,9 @@
 function obtenerElementoAleatorio(array) {
-   // Devuelve un elemento aleatorio del arreglo array.
-   // PISTA: Usa el método Math.random().
-   // Tu código:
+  // Devuelve un elemento aleatorio del arreglo array.
+  // PISTA: Usa el método Math.random().
+  // Tu código:
+  let indice = Math.floor(Math.random() * (array.length - 1));
+  return array[indice];
 }
 
 module.exports = obtenerElementoAleatorio;
