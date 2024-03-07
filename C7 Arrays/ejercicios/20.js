5c0bdb054f78a65e4eb110805bf7c0389f16d22e
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-07 13:43:13 -0500

    C7 -> 20.js solucionado

--- C7 Arrays/ejercicios/20.js
+++ C7 Arrays/ejercicios/20.js
@@ -2,6 +2,18 @@ function todosIguales(array) {
   // Si todos los elementos del arreglo son iguales, retornar true.
   // Caso contrario, retornar false.
   // Tu código:
+  var copy = array.slice();
+  var result = true;
+  array.forEach((x, index) => {
+    copy.forEach((y) => {
+      if (x != y) {
+        result = false;
+        return;
+      }
+    });
+  });
+
+  return result;
 }
 
 module.exports = todosIguales;
