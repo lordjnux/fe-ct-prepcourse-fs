7c254c3abd20c29dbbea1f7d22b2c5bc1371bde0
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-08 09:52:07 -0500

    C8 Objects -> 20.js solucionado

--- C8 Objects + Callbacks/objects/ejercicios/20.js
+++ C8 Objects + Callbacks/objects/ejercicios/20.js
@@ -4,6 +4,11 @@ function sumarLikesDeUsuario(objetoUsuario) {
   // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
   // Debes sumar los likes de todos los post y retornar el resultado.
   // Tu código:
+  let sumaLikes = 0;
+  objetoUsuario.posts.forEach((post) => {
+    sumaLikes += post.likes;
+  });
+  return sumaLikes;
 }
 
 module.exports = sumarLikesDeUsuario;
