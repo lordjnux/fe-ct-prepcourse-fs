7f0a7f99ac704fc41d78a37c0e1507d18afade3c
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-08 10:33:24 -0500

    C8 Objects -> 22.js solucionado

--- C8 Objects + Callbacks/objects/ejercicios/22.js
+++ C8 Objects + Callbacks/objects/ejercicios/22.js
@@ -1,10 +1,19 @@
 function esAnagrama(str1, str2) {
-   // La función recibe dos argumentos "str1" y "str2" que son strings.
-   // Determina si los dos strings son anagramas.
-   // Devuelve true de ser así, sino retorna false.
-   // IMPORTANTE: Un anagrama es una palabra que se forma
-   // con las mismas letras que otra, pero en orden diferente.
-   // Tu código:
+  // La función recibe dos argumentos "str1" y "str2" que son strings.
+  // Determina si los dos strings son anagramas.
+  // Devuelve true de ser así, sino retorna false.
+  // IMPORTANTE: Un anagrama es una palabra que se forma
+  // con las mismas letras que otra, pero en orden diferente.
+  // Tu código:
+
+  return (
+    str1
+      .toUpperCase()
+      .split("")
+      .every((letra) => {
+        return str2.toUpperCase().split("").includes(letra);
+      }) && str1.length == str2.length
+  );
 }
 
 module.exports = esAnagrama;
