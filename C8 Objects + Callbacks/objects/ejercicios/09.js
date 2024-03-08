cfe646a0e0b47058a37113edf54aa2b2117beda9
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-08 09:25:29 -0500

    C8 Objects -> 09.js solucionado

--- C8 Objects + Callbacks/objects/ejercicios/09.js
+++ C8 Objects + Callbacks/objects/ejercicios/09.js
@@ -3,6 +3,11 @@ function nuevoUsuario(nombre, email, password) {
   // Este debe tener las propiedades: "nombre", "email" y "password" con sus respectivos valores.
   // Retorna el objeto creado.
   // Tu código:
+  let usuario = {};
+  usuario.nombre = nombre;
+  usuario.email = email;
+  usuario.password = password;
+  return usuario;
 }
 
 module.exports = nuevoUsuario;
