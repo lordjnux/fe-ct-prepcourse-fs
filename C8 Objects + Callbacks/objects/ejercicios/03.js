4e4a943f2c50585411c07bc19f4ed033dfb081b8
Jeroham.Sanchez <jeroham.sanchez@gmail.com>
2024-03-08 08:59:13 -0500

    C8 Objects -> 03.js solucionado

--- C8 Objects + Callbacks/objects/ejercicios/03.js
+++ C8 Objects + Callbacks/objects/ejercicios/03.js
@@ -1,6 +1,9 @@
 const agregarNuevaPropiedad = (objeto, propiedad, valor) => {
   // Añade una nueva propiedad al objeto con su respectivo valor pasado por la función.
   // Tu código:
+  let obj = objeto;
+  obj[propiedad] = valor;
+  return obj;
 };
 
 module.exports = agregarNuevaPropiedad;
